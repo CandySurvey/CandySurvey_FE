8cf576b7205db03532f318e691bd571038d2d614
qkrvudtn98 <ps3624@naver.com>
2023-02-22 00:34:45 +0900

    회원가입 페이지 - 기능 구현 X

---
+++ app/src/main/java/com/flatwater/candysurvey/auth/SignUpActivity.kt
@@ -0,0 +1,21 @@
+package com.flatwater.candysurvey.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.flatwater.candysurvey.R
+import com.flatwater.candysurvey.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySignUpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+
+
+
+    }
+}

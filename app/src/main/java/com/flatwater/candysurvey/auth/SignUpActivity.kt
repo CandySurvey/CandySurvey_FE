419dfbff1baf66487cc8fc9f0f3e4ddf2936c16f
qkrvudtn98 <ps3624@naver.com>
2023-02-23 22:48:52 +0900

    회원가입 페이지 버튼 이벤트 구현 (미완)

--- app/src/main/java/com/flatwater/candysurvey/auth/SignUpActivity.kt
+++ app/src/main/java/com/flatwater/candysurvey/auth/SignUpActivity.kt
@@ -1,7 +1,9 @@
 package com.flatwater.candysurvey.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.flatwater.candysurvey.R
 import com.flatwater.candysurvey.databinding.ActivitySignUpBinding
@@ -11,11 +13,24 @@ class SignUpActivity : AppCompatActivity() {
     private lateinit var binding : ActivitySignUpBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
 
+        // 다음 버튼 클릭했을 때 이벤트 핸들링 (약관동의 페이지로 넘어가야 함)
+        binding.nextBtn.setOnClickListener {
+            val intent = Intent(this, TermsOfServiceActivity::class.java)
+            startActivity(intent)
+        }
 
+        binding.closeBtn.setOnClickListener {
+            // 이전 페이지로 되돌아가는 기능 구현 예정
+            Toast.makeText(this, "되돌아갑니다.", Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
+
+// 닉네임 겹치는 경우, 아이디, 비밀번호 겹치거나 양식 맞추지 않았을 때 경고창 띄워주는 기능 추가 예정
+// 닉네임/아이디/비밀번호 입력 안했을 때 경고 메세지 띄워주는 기능 추가해야 함

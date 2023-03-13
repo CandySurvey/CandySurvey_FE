8dec6c233f991559ac1b484b82922449051e5066
qkrvudtn98 <ps3624@naver.com>
2023-03-13 20:11:29 +0900

    약관동의 페이지 뒤로가기 기능 추가 (미완)

--- app/src/main/java/com/flatwater/candysurvey/auth/TermsOfServiceActivity.kt
+++ app/src/main/java/com/flatwater/candysurvey/auth/TermsOfServiceActivity.kt
@@ -3,6 +3,7 @@ package com.flatwater.candysurvey.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.flatwater.candysurvey.R
 import com.flatwater.candysurvey.databinding.ActivityTermsOfServiceBinding
@@ -20,12 +21,18 @@ class TermsOfServiceActivity : AppCompatActivity() {
         binding.termsOfServiceBtn.setOnClickListener{
             // 약관 내용 담은 프레그먼트 애니메이션 추가 예정
             // 약관 내용 마크업 해야함
+            Toast.makeText(this, "약관동의 페이지 띄워야함..", Toast.LENGTH_SHORT).show()
         }
 
         // 약관 동의 버튼 이벤트 핸들링 (메인 페이지로 이동하는 스플래시 화면으로 이동)
         binding.startBtn.setOnClickListener {
             val intent = Intent(this, LoginCompleteSplashActivity::class.java)
             startActivity(intent)
+        }
+
+        // 뒤로가기 버튼
+        binding.closeBtn.setOnClickListener {
+            finish()
         }
 
     }

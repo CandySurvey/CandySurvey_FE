067542269d0a2625c97c20ee112b3e3e08585046
qkrvudtn98 <ps3624@naver.com>
2023-03-13 20:10:01 +0900

    환경설정 페이지 동적 페이지 생성 (시작 전)

---
+++ app/src/main/java/com/flatwater/candysurvey/setting/SettingActivity.kt
@@ -0,0 +1,20 @@
+package com.flatwater.candysurvey.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.flatwater.candysurvey.R
+import com.flatwater.candysurvey.databinding.ActivitySettingBinding
+
+class SettingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_setting)
+
+    }
+}

259e6811fae05844f393392f66ab5aed81ec39d8
qkrvudtn98 <ps3624@naver.com>
2023-03-13 20:05:11 +0900

    로컬 로그인 페이지 뒤로가기 기능 / 아이디 비밀번호 찾기 버튼 기능 추가 (미완)

--- app/src/main/java/com/flatwater/candysurvey/auth/LocalLoginActivity.kt
+++ app/src/main/java/com/flatwater/candysurvey/auth/LocalLoginActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.flatwater.candysurvey.MainActivity
 import com.flatwater.candysurvey.R
 import com.flatwater.candysurvey.databinding.ActivityLocalLoginBinding
 
@@ -12,42 +13,58 @@ class LocalLoginActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityLocalLoginBinding
 
+    var backPressedTime : Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_local_login)
 
-        // 구글 로그인 버튼 클릭 이벤트 (임시)
-        binding.googleLoginBtn.setOnClickListener {
-
-            Toast.makeText(this, "구글 소셜 로그인 연동입니다", Toast.LENGTH_SHORT).show()
-
-        }
-
-        // 카카오 로그인 버튼 클릭 이벤트 (임시)
-        binding.kakaoLoginBtn.setOnClickListener {
-
-            Toast.makeText(this, "카카오 소셜 로그인 연동입니다", Toast.LENGTH_SHORT).show()
-
-        }
-
-        // 네이버 로그인 버튼 클릭 이벤트 (임시)
-        binding.naverLoginBtn.setOnClickListener {
-
-            Toast.makeText(this, "네이버 소셜 로그인 연동입니다", Toast.LENGTH_SHORT).show()
-
-        }
-
         binding.closeBtn.setOnClickListener {
             // 되돌아가기 기능 구현예정
             Toast.makeText(this, "되돌아가기 기능 구현 예정", Toast.LENGTH_SHORT).show()
-
         }
+
+        // 아이디 찾기 버튼 클릭 이벤트 구현 (완성)
+        binding.findId.setOnClickListener {
+            val intent = Intent(this, FindIdActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 비밀번호 찾기 버튼 클릭 이벤트 구현 (완성)
+        binding.findPw.setOnClickListener {
+            val intent = Intent(this, FindPasswordActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 뒤로가기 버튼 구현(완성)
+        binding.closeBtn.setOnClickListener {
+            val intent = Intent(this, IntroActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        
+        // 로그인 버튼 구현 (완성)
+        binding.localLoginBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onBackPressed() {
+        if(backPressedTime + 3000 > System.currentTimeMillis()){
+            super.onBackPressed()
+            finishAffinity()
+        } else {
+            Toast.makeText(this, "정말 종료하시겠습니까?", Toast.LENGTH_SHORT).show()
+        }
+
+        backPressedTime = System.currentTimeMillis()
     }
 }
 
-// 아이디/비밀번호 입력할 때 소셜로그인 부분 스크롤 기능 구현 (키패드가 가리니까)
 // 아이디 찾기/비밀번호 찾기 버튼 태그로 바꾸고 클릭 이벤트 구현해야함
 
 

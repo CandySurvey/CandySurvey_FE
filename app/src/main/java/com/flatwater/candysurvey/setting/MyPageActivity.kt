170a3a4b119d4de73bc27833f00f17d5c3214667
qkrvudtn98 <ps3624@naver.com>
2023-03-14 19:26:07 +0900

    마이페이지 기능 구현 - 갤러리 연동 / 다이얼로그 (미완)

--- app/src/main/java/com/flatwater/candysurvey/setting/MyPageActivity.kt
+++ app/src/main/java/com/flatwater/candysurvey/setting/MyPageActivity.kt
@@ -1,8 +1,18 @@
 package com.flatwater.candysurvey.setting
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
+import com.flatwater.candysurvey.MainActivity
 import com.flatwater.candysurvey.R
 import com.flatwater.candysurvey.databinding.ActivityMyPageBinding
 
@@ -11,9 +21,42 @@ class MyPageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMyPageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_my_page)
 
+        binding.closeBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.galleryBtn.setOnClickListener{
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            activityResult.launch(intent)
+        }
+
+        // Dialog 버튼 이벤트 설정
+        val dialog = CustomDialog(this)
+        binding.editBtn.setOnClickListener {
+            dialog.showDialog()
+        }
+
+    }
+
+    // 갤러리 어플 연동 소스코드
+    private val activityResult: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()){
+        if(it.resultCode == RESULT_OK && it.data != null){
+            val uri = it.data!!.data
+            Glide.with(this)
+                .load(uri)
+                .into(binding.galleryBtn)
+        }
     }
 }
+
+// 추가 기능 구현
+// - 닉네임 변경하면 서버에 있는 닉네임까지 변경할 수 있도록 설정 (로컬 로그인일 경우에 한정)

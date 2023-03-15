a18d241eb0d855431030063f002878443fa415d4
qkrvudtn98 <ps3624@naver.com>
2023-03-15 23:54:00 +0900

    Dialog 테두리 영역 설정 (미완)

--- app/src/main/java/com/flatwater/candysurvey/setting/CustomDialog.kt
+++ app/src/main/java/com/flatwater/candysurvey/setting/CustomDialog.kt
@@ -2,11 +2,11 @@ package com.flatwater.candysurvey.setting
 
 import android.app.Dialog
 import android.content.Context
-import android.content.DialogInterface.OnClickListener
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.Window
 import android.view.WindowManager
 import android.widget.Button
-import android.widget.Toast
-import androidx.core.content.contentValuesOf
 import com.flatwater.candysurvey.R
 
 class CustomDialog(context: Context) {
@@ -14,14 +14,18 @@ class CustomDialog(context: Context) {
     private val dialog = Dialog(context)
 
     fun showDialog(){
+        // Dialog 테두리 설정
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+
         // Dialog와 레이아웃 연결
         dialog.setContentView(R.layout.nickname_edit_dialog)
 
         // Dialog 크기 설정
         dialog.window!!.setLayout(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT)
 
-        // Dialog 영역 밖을 터치했을 때, Dialog가 사라지도록 하는 부분
-        dialog.setCanceledOnTouchOutside(true)
+        // Dialog 영역 밖을 터치했을 때, Dialog가 사라지도록 하는 부분 ( true -> 사라짐 / false -> 사라지지 않음 )
+        dialog.setCanceledOnTouchOutside(false)
 
         // 취소가 가능하도록 하는 부분
         dialog.setCancelable(true)
@@ -37,6 +41,7 @@ class CustomDialog(context: Context) {
         }
 
         noBtn.setOnClickListener {
+            // '아니오' 터치시 화면상 변화 없음
             dialog.dismiss()
         }
     }

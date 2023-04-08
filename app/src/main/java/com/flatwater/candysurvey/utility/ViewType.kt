586498b5aab64ff346e2ce25cff055a8fdc11d48
qkrvudtn98 <ps3624@naver.com>
2023-04-09 07:38:07 +0900

    설문 템플릿 뷰타입 설정 (미완성)

---
+++ app/src/main/java/com/flatwater/candysurvey/utility/ViewType.kt
@@ -0,0 +1,10 @@
+package com.flatwater.candysurvey.utility
+
+enum class ViewType(name: String){
+    TITLE("TITLE"),
+    CONTOUR("CONTOUR"),
+    SHORT_ANS("SHORT_ANS"),
+    LONG_ANS("LONG_ANS"),
+    OPTIONAL("OPTIONAL"),
+    DROPDOWN("DROPDOWN")
+}

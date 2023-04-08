d22571b81911cb6fe28aa19bea0e9b089a2f30cc
qkrvudtn98 <ps3624@naver.com>
2023-04-09 07:42:13 +0900

    설문 템플릿 데이터에 맞춰서 랜더링 시도 (미완)

--- app/src/main/java/com/flatwater/candysurvey/produce/ProduceActivity.kt
+++ app/src/main/java/com/flatwater/candysurvey/produce/ProduceActivity.kt
@@ -5,19 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.flatwater.candysurvey.MainActivity
 import com.flatwater.candysurvey.R
+import com.flatwater.candysurvey.adapter.CommonAdapter
 import com.flatwater.candysurvey.databinding.ActivityProduceBinding
 import com.flatwater.candysurvey.setting.MyPageActivity
 import com.flatwater.candysurvey.template.TitleTemplateModel
 import com.flatwater.candysurvey.template.TitleTemplateRVAdapter
+import com.flatwater.candysurvey.templatedata.CommonItem
+import com.flatwater.candysurvey.templatedata.viewobject.LongAnswerViewObject
+import com.flatwater.candysurvey.templatedata.viewobject.ShortAnswerViewObject
+import com.flatwater.candysurvey.templatedata.viewobject.TitleViewObject
+import com.flatwater.candysurvey.templatedata.viewobject.ViewObject
+import com.flatwater.candysurvey.viewholder.TitleViewHolder
 
 class ProduceActivity : AppCompatActivity() {
 
@@ -35,9 +41,7 @@ class ProduceActivity : AppCompatActivity() {
 
         // 뒤로가기 버튼 연결
         binding.closeBtn.setOnClickListener{
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
+            showBackDialog()
         }
 
         binding.saveBtn.setOnClickListener {
@@ -48,7 +52,7 @@ class ProduceActivity : AppCompatActivity() {
         val rv_default = findViewById<RecyclerView>(R.id.surveyArea)
         val itemList = ArrayList<TitleTemplateModel>()
 
-        itemList.add(TitleTemplateModel("candy", ""))
+        itemList.add(TitleTemplateModel("$surveyTitle", ""))
 
         val templateTitleAdapter = TitleTemplateRVAdapter(itemList)
         templateTitleAdapter.notifyDataSetChanged()
@@ -82,8 +86,22 @@ class ProduceActivity : AppCompatActivity() {
             Toast.makeText(this, "공유하기 템플릿 띄워야함..", Toast.LENGTH_SHORT).show()
         }
 
+        // 설문 문항 영역 템플릿 데이터 적용되는지 체크
+        val adapter = CommonAdapter (
+            arrayOf(
+                CommonItem(
+                    "TITLE",
+                    TitleViewObject(null, null )
+                ), CommonItem(
+                    "SHORT_ANS",
+                    ShortAnswerViewObject(null)
+                )
+            )
+        )
+        binding.surveyArea.adapter = adapter
     }
 
+    // 저장 버튼 터치시 나타다는 다이얼로그
     private fun showSaveDialog(){
 
         val saveDialogView = LayoutInflater.from(this).inflate(R.layout.save_check_dialog, null)
@@ -98,9 +116,33 @@ class ProduceActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
         alertSaveDialog.findViewById<Button>(R.id.noBtn)?.setOnClickListener {
             // 원래 작업하던 페이지로 돌아가게 구현해야함!
             Toast.makeText(this,"원래대로 돌아갑니다!", Toast.LENGTH_SHORT).show()
+            alertSaveDialog.dismiss()
+        }
+    }
+
+    // 뒤로가기 버튼 터치시 나타나는 다이얼로그
+    private fun showBackDialog(){
+
+        val backDialogView = LayoutInflater.from(this).inflate(R.layout.back_check_dialog, null)
+        val backBuilder = AlertDialog.Builder(this)
+            .setView(backDialogView)
+
+        val alertBackDialog = backBuilder.show()
+
+        alertBackDialog.findViewById<Button>(R.id.yesBtn)?.setOnClickListener {
+            Toast.makeText(this, "저장하지 않고 메인페이지로 되돌아갑니다!", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        alertBackDialog.findViewById<Button>(R.id.noBtn)?.setOnClickListener {
+            Toast.makeText(this, "다시 작업하던 화면으로 되돌아갑니다!", Toast.LENGTH_SHORT).show()
+            alertBackDialog.dismiss()
         }
     }
 

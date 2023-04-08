76ecb472772752899cd235a16ce3e5eb6d2b2579
qkrvudtn98 <ps3624@naver.com>
2023-04-09 07:37:34 +0900

    설문 템플릿 공통 데이터 양식 (완료)

---
+++ app/src/main/java/com/flatwater/candysurvey/templatedata/CommonItem.kt
@@ -0,0 +1,8 @@
+package com.flatwater.candysurvey.templatedata
+
+import com.flatwater.candysurvey.templatedata.viewobject.ViewObject
+
+data class CommonItem (
+    val questionType : String,
+    val questionObject : ViewObject
+)

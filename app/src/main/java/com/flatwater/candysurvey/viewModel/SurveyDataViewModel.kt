e1b71a89e9b330a44a416a9d946d7011a75af3e9
qkrvudtn98 <ps3624@naver.com>
2023-05-02 22:22:36 +0900

    ViewModel 적용할 데이터 설정하는 viewModel (미완)

---
+++ app/src/main/java/com/flatwater/candysurvey/viewModel/SurveyDataViewModel.kt
@@ -0,0 +1,10 @@
+package com.flatwater.candysurvey.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.flatwater.candysurvey.templatedata.viewobject.TitleViewObject
+
+class SurveyDataViewModel : ViewModel() {
+
+}

b4377b7a6c93ab87ed149759a4b890783c9fa3c4
qkrvudtn98 <ps3624@naver.com>
2023-05-03 23:37:24 +0900

    제목 템플릿 데이터 livedata로 받아오는 작업 코드 (미완)

--- app/src/main/java/com/flatwater/candysurvey/viewModel/SurveyDataViewModel.kt
+++ app/src/main/java/com/flatwater/candysurvey/viewModel/SurveyDataViewModel.kt
@@ -1,10 +1,19 @@
 package com.flatwater.candysurvey.viewModel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.flatwater.candysurvey.template.TitleTemplateModel
 import com.flatwater.candysurvey.templatedata.viewobject.TitleViewObject
+import kotlinx.coroutines.launch
 
 class SurveyDataViewModel : ViewModel() {
+
+    private val _titleTemplateData = MutableLiveData<TitleViewObject>()
+
+    val resultTitleTemplateData : LiveData<TitleViewObject>
+        get() = _titleTemplateData
 
 }

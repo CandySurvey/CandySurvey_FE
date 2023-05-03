f44cda19e6d5e8850fb4bb82c894de91560d4bb2
qkrvudtn98 <ps3624@naver.com>
2023-05-03 23:37:55 +0900

    editable import 제거

--- app/src/main/java/com/flatwater/candysurvey/templatedata/viewobject/TitleViewObject.kt
+++ app/src/main/java/com/flatwater/candysurvey/templatedata/viewobject/TitleViewObject.kt
@@ -1,7 +1,5 @@
 package com.flatwater.candysurvey.templatedata.viewobject
 
-import android.text.Editable
-
 data class TitleViewObject (
     val surveyTitle : String?,
     val surveyDetail : String?,

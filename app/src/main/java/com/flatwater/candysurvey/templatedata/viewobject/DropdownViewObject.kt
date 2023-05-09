a4f2d4bc3ad28d4283e8b3223e50488781380e79
qkrvudtn98 <ps3624@naver.com>
2023-05-09 23:53:09 +0900

    null 허용

--- app/src/main/java/com/flatwater/candysurvey/templatedata/viewobject/DropdownViewObject.kt
+++ app/src/main/java/com/flatwater/candysurvey/templatedata/viewobject/DropdownViewObject.kt
@@ -3,6 +3,6 @@ package com.flatwater.candysurvey.templatedata.viewobject
 import android.text.Editable
 
 data class DropdownViewObject (
-    val questionTitle : String,
+    val questionTitle : String?,
     // questionItem 하위 속성 선언해주는 방법 알아내야함!
 ) : ViewObject()

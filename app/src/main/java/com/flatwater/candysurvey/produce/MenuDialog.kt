72b425aeb4bf549a1af01baeb5afbb895f2dec3c
qkrvudtn98 <ps3624@naver.com>
2023-05-09 23:54:27 +0900

    menuDialog에서 template별 클릭이벤트 구현 (questionType 전달)

--- app/src/main/java/com/flatwater/candysurvey/produce/MenuDialog.kt
+++ app/src/main/java/com/flatwater/candysurvey/produce/MenuDialog.kt
@@ -2,7 +2,9 @@ package com.flatwater.candysurvey.produce
 
 import android.app.Dialog
 import android.content.Context
+import android.util.Log
 import android.view.WindowManager
+import android.widget.LinearLayout
 import com.flatwater.candysurvey.R
 
 class MenuDialog(context: Context) {
@@ -20,6 +22,72 @@ class MenuDialog(context: Context) {
 
         menuDialog.show()
 
+        // template button
+        val titleBtn = menuDialog.findViewById<LinearLayout>(R.id.titleBtn)
+        titleBtn.setOnClickListener {
+            var templateType = "TITLE"
+            onClickedListener.onClicked(templateType)
+            menuDialog.dismiss()
+        }
+
+        val contourBtn = menuDialog.findViewById<LinearLayout>(R.id.contourBtn)
+        contourBtn.setOnClickListener {
+            var templateType = "CONTOUR"
+            onClickedListener.onClicked(templateType)
+            menuDialog.dismiss()
+        }
+
+        val shortAnswerBtn = menuDialog.findViewById<LinearLayout>(R.id.shortAnswerBtn)
+        shortAnswerBtn.setOnClickListener {
+            var templateType = "SHORT_ANS"
+            onClickedListener.onClicked(templateType)
+            menuDialog.dismiss()
+        }
+
+        val longAnswerBtn = menuDialog.findViewById<LinearLayout>(R.id.longAnswerBtn)
+        longAnswerBtn.setOnClickListener {
+            var templateType = "LONG_ANS"
+            onClickedListener.onClicked(templateType)
+            menuDialog.dismiss()
+        }
+
+        val multipleChoiceBtn = menuDialog.findViewById<LinearLayout>(R.id.multipleChoiceBtn)
+        multipleChoiceBtn.setOnClickListener {
+            var templateType = "OPTIONAL"
+            onClickedListener.onClicked(templateType)
+            menuDialog.dismiss()
+        }
+
+        val dropdownBtn = menuDialog.findViewById<LinearLayout>(R.id.dropDownBtn)
+        dropdownBtn.setOnClickListener {
+            var templateType = "DROPDOWN"
+            onClickedListener.onClicked(templateType)
+            menuDialog.dismiss()
+        }
+
+        // 나머지 템플릿들은 XML에서 구현하고 연결시켜줄 예정임!
+        val starBtn = menuDialog.findViewById<LinearLayout>(R.id.starBtn)
+        val linearMagnificationBtn = menuDialog.findViewById<LinearLayout>(R.id.linearMagnificationBtn)
+        val timeBtn = menuDialog.findViewById<LinearLayout>(R.id.timeBtn)
+        val dateBtn = menuDialog.findViewById<LinearLayout>(R.id.dateBtn)
+        val emailBtn = menuDialog.findViewById<LinearLayout>(R.id.emailBtn)
+        val phoneNumberBtn = menuDialog.findViewById<LinearLayout>(R.id.phoneNumberBtn)
+        val numericBtn = menuDialog.findViewById<LinearLayout>(R.id.numericBtn)
+        val insertImageBtn = menuDialog.findViewById<LinearLayout>(R.id.insertImageBtn)
+        val insertVideoBtn = menuDialog.findViewById<LinearLayout>(R.id.insertVideoBtn)
+        val fileBtn = menuDialog.findViewById<LinearLayout>(R.id.fileBtn)
+
+
+    }
+
+    interface ButtonClickListener {
+        fun onClicked(questionType : String)
+    }
+
+    private lateinit var onClickedListener: ButtonClickListener
+
+    fun setOnClickedListener(listener: ButtonClickListener){
+        onClickedListener = listener
     }
 
 }

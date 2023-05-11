e2ae4c2b80cb81f1c0286ef9027769e359311488
qkrvudtn98 <ps3624@naver.com>
2023-05-11 22:55:25 +0900

    ViewModel 구조 변경 (미완)

--- app/src/main/java/com/flatwater/candysurvey/viewModel/SurveyDataViewModel.kt
+++ app/src/main/java/com/flatwater/candysurvey/viewModel/SurveyDataViewModel.kt
@@ -5,19 +5,32 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.flatwater.candysurvey.template.TitleTemplateModel
+import com.flatwater.candysurvey.api.RetrofitInstance
+import com.flatwater.candysurvey.api.SurveyApi
+import com.flatwater.candysurvey.jsondata.*
 import com.flatwater.candysurvey.templatedata.viewobject.TitleViewObject
 import kotlinx.coroutines.launch
 
 class SurveyDataViewModel : ViewModel() {
 
-    private val _titleTemplateData = MutableLiveData<TitleViewObject>()
+    val retrofitInstance : SurveyApi = RetrofitInstance.getInstance().create(SurveyApi::class.java)
 
-    val resultTitleTemplateData : LiveData<TitleViewObject>
-        get() = _titleTemplateData
-
-    fun printData() {
-        Log.d("LiveData", "$resultTitleTemplateData")
+    fun requestPost() = viewModelScope.launch {
+        val postData = SurveyDataModel(Survey("test code", "test1", Questions(listOf(Question("title", "first title", "first detail", listOf(Items("test item")))))))
+        try {
+            val response = retrofitInstance.requestPost(postData)
+            if (response.isSuccessful) {
+                val responseData = response.body()
+                Log.d("Server", "성공!")
+            } else {
+                Log.d("Server", "실패!")
+            }
+        } catch (e:Exception) {
+            Log.d("Post", "예외처리")
+        }
     }
 
+//    fun getAllSurvey() = viewModelScope.launch {
+//        Log.d("JSON Data", retrofitInstance.getAllSurveys().toString())
+//    }
 }

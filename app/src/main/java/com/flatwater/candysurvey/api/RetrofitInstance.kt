41c3dde9146c525606ef5bfcc1f5848b56d9e756
qkrvudtn98 <ps3624@naver.com>
2023-05-11 22:02:47 +0900

    RetrofitInstance 서비스 객체 생성

---
+++ app/src/main/java/com/flatwater/candysurvey/api/RetrofitInstance.kt
@@ -0,0 +1,21 @@
+package com.flatwater.candysurvey.api
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import retrofit2.Retrofit
+
+object RetrofitInstance {
+
+    val BASE_URL = "http://3.35.144.196:8080/" // Server 주소 입력
+
+    val client = Retrofit
+        .Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(Json.asConverterFactory(MediaType.parse("application/json")!!))
+        .build()
+
+    fun getInstance() : Retrofit {
+        return client
+    }
+}

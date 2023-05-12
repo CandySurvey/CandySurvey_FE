97ec4684fa51185d3040a446a4ff40c138204dc4
qkrvudtn98 <ps3624@naver.com>
2023-05-12 21:39:08 +0900

    템플릿 추가, 저장에 따른 JSON Data 형식 적용 코드 추가

--- app/src/main/java/com/flatwater/candysurvey/produce/ProduceActivity.kt
+++ app/src/main/java/com/flatwater/candysurvey/produce/ProduceActivity.kt
@@ -1,5 +1,6 @@
 package com.flatwater.candysurvey.produce
 
+import android.content.ClipData.Item
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -40,27 +41,42 @@ class ProduceActivity : AppCompatActivity() {
     private var survey : Survey = Survey("", "", questions)
     private var surveyJson : SurveyDataModel = SurveyDataModel(survey)
 
+    private val firstTitleTemplate = SurveyDataModel(
+        Survey(
+            surveyTitle = "$surveyTitle",
+            surveyDetail = "",
+            questions = Questions(
+                listOf(Question(
+                    questionType = "TITLE",
+                    questionTitle = "첫번째 설문입니다!",
+                    questionDetail = "",
+                    listOf(Items(""))
+                ))
+            )
+        )
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_produce)
 
-        // MainActivity에서 SurveyTitle Data 받아오는 코드
+        // Get Survey Title name at MainPage
         surveyTitle = intent.getStringExtra("surveyTitle")
         binding.surveyTitle.text = surveyTitle
 
-        // 뒤로가기 버튼 연결
+        // Dialog : Back Button
         binding.closeBtn.setOnClickListener{
             showBackDialog()
         }
 
-        // 메인페이지 네비게이션 연결
+        // Navigation : MainPage
         binding.navigateMainPage.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
 
-        // 마이페이지 네비게이션 연결
+        // Navigation : MyPage
         binding.navigateMyPage.setOnClickListener {
             val intent = Intent(this, MyPageActivity::class.java)
             startActivity(intent)
@@ -70,6 +86,7 @@ class ProduceActivity : AppCompatActivity() {
         // 초기 제목 템플릿 자동으로 연결
         val rv_default = findViewById<RecyclerView>(R.id.surveyArea)
         dataSet.add(CommonItem("TITLE", TitleViewObject(null, null)))
+        Log.d("JSON Data", "$dataSet")
 
         val adapter = CommonAdapter(dataSet)
         adapter.notifyDataSetChanged()
@@ -77,36 +94,44 @@ class ProduceActivity : AppCompatActivity() {
         rv_default.adapter = adapter
         rv_default.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
-        // 템플릿 다이얼로그 연결
+        // Navigation : Template Menu Dialog
         val menuDialog = MenuDialog(this)
         binding.plusBtn.setOnClickListener {
             menuDialog.showDialog()
 
             // questionType에 따라서 다른 Recyclerview 랜더링 해야함!
             menuDialog.setOnClickedListener(object : MenuDialog.ButtonClickListener{
+
                 override fun onClicked(questionType : String) {
+
+                    val emptyQuestion = Question(questionType, "", "", listOf(item))
+                    questions = firstTitleTemplate.survey.questions
+
                     when(questionType){
                         "TITLE" -> {
                             dataSet.add(CommonItem(questionType, TitleViewObject(null, null)))
-//                            Log.d("Json Data", "${dataSet[0]}")
+                            questions = addQuestion(questions, emptyQuestion)
                             adapter.notifyDataSetChanged()
-
                         }
                         "CONTOUR" -> Log.d("JSON Data", "구분선 템플릿 호출!")
                         "SHORT_ANS" -> {
                             dataSet.add(CommonItem(questionType, ShortAnswerViewObject(null)))
+                            questions = addQuestion(questions, emptyQuestion)
                             adapter.notifyDataSetChanged()
                         }
                         "LONG_ANS" -> {
                             dataSet.add(CommonItem(questionType, LongAnswerViewObject(null)))
+                            questions = addQuestion(questions, emptyQuestion)
                             adapter.notifyDataSetChanged()
                         }
                         "OPTIONAL" -> {
                             dataSet.add(CommonItem(questionType, OptionalViewObject(null)))
+                            questions = addQuestion(questions, emptyQuestion)
                             adapter.notifyDataSetChanged()
                         }
                         "DROPDOWN" -> {
                             dataSet.add(CommonItem(questionType, DropdownViewObject(null)))
+                            questions = addQuestion(questions, emptyQuestion)
                             adapter.notifyDataSetChanged()
                         }
                     }
@@ -114,20 +139,26 @@ class ProduceActivity : AppCompatActivity() {
             })
         }
 
-        // 저장 버튼 연결
+        // Save Button
         binding.saveBtn.setOnClickListener {
+
             showSaveDialog()
             adapter.notifyDataSetChanged()
+
+            // Question Data를 추가한 결과 초기화 & Data Model Json Data로 변환
+            survey = Survey("$surveyTitle", "", questions)
+            surveyJson = SurveyDataModel(survey)
+            Log.d("JSON Data", "${Json.encodeToString(surveyJson)}")
+            Log.d("JSON Data", "$dataSet")
 
             for(i in 0..(dataSet.size-1) ){
 
                 Log.d("Json Data", "${dataSet[i]}")
 
-                // CommonItem으로 접근하여 데이터 분리해서 SurveyDataModel에 연결하는 코드가 들어가야함!
+                // CommonItem으로 접근하여 데이터 분리해서 SurveyDataModel에 연결하는 코드가 들어가야함! (중요)
                 when(dataSet[i].questionType) {
                     "TITLE" -> {
                         Log.d("JSON Data", "제목 템플릿입니다!")
-//                        Log.d("JSON Data", "${dataSet[i].questionObject}")
                     }
                     "CONTOUR" -> {
                         Log.d("JSON Data", "구분선 템플릿입니다!")
@@ -148,30 +179,19 @@ class ProduceActivity : AppCompatActivity() {
             }
         }
 
-        // 공유하기 버튼 연결
+        // Navigation : Share Button
         binding.shareBtn.setOnClickListener {
             // 공유하기 템플릿 만드는 로직이 들어가야 합니다.
             Toast.makeText(this, "공유하기 템플릿 띄워야함..", Toast.LENGTH_SHORT).show()
         }
 
-        // 샘플 Json Data 설정 코드 (Retrofit2 적용해야함)
-        item = Items("")
-        question = Question("Title", "첫번째 설문입니다!", "", listOf(item))
-        questions = Questions(listOf(question))
-        Log.d("JSON Data", "$questions")
-
-        survey = Survey("$surveyTitle", "첫번째 문제입니다!", questions)
-        surveyJson = SurveyDataModel(survey)
-        val surveyJsonData = Json.encodeToString(surveyJson)
-        Log.d("JSON Data", surveyJsonData)
-
-        // ViewModel 선언 & POST Request Test Code
+        // ViewModel Declaration & POST Request Test Code
         val viewModel = ViewModelProvider(this).get(SurveyDataViewModel::class.java)
         viewModel.requestPost()
 
     }
 
-    // 저장 버튼 터치시 나타다는 다이얼로그
+    // Dialog : Save Button
     private fun showSaveDialog(){
 
         val saveDialogView = LayoutInflater.from(this).inflate(R.layout.save_check_dialog, null)
@@ -194,7 +214,7 @@ class ProduceActivity : AppCompatActivity() {
         }
     }
 
-    // 뒤로가기 버튼 터치시 나타나는 다이얼로그
+    // Dialog : Back Button
     private fun showBackDialog(){
 
         val backDialogView = LayoutInflater.from(this).inflate(R.layout.back_check_dialog, null)
@@ -214,6 +234,14 @@ class ProduceActivity : AppCompatActivity() {
             Toast.makeText(this, "다시 작업하던 화면으로 되돌아갑니다!", Toast.LENGTH_SHORT).show()
             alertBackDialog.dismiss()
         }
+    }
+
+    // Function : Add Question Object
+    private fun addQuestion(questions: Questions, newQuestion: Question) : Questions {
+        val addedQuestionList = questions.question.toMutableList().apply {
+            add(newQuestion)
+        }
+        return Questions(addedQuestionList)
     }
 
 }
